6a47e0cd034e8666e389c11a08701be1184b37eb
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-06-22 07:08:53 +0800

    Add tables db

--- server/kpri_kd.sql
+++ server/kpri_kd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 19, 2024 at 11:19 PM
+-- Generation Time: Jun 21, 2024 at 11:06 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -24,6 +24,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `balance_savings`
+--
+
+CREATE TABLE `balance_savings` (
+  `id` int NOT NULL,
+  `user_member_id` int NOT NULL,
+  `principal_saving` decimal(10,2) DEFAULT '0.00',
+  `mandatory_saving` decimal(10,2) DEFAULT NULL,
+  `voluntary_saving` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `balance_savings`
+--
+
+INSERT INTO `balance_savings` (`id`, `user_member_id`, `principal_saving`, `mandatory_saving`, `voluntary_saving`) VALUES
+(1, 1, 10000.00, 3400000.00, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` int NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` text,
+  `status` enum('active','inactive') DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `image_url`, `title`, `description`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'htttps/sdasdsad.com', 'judul sdasd', 'asdasda', 'inactive', '2024-06-21 10:50:13', '2024-06-21 10:50:13'),
+(2, 'https/img/a.com', 'judul ababa', 'sadnafjaebijfbsdijgbseidjgbisdjfb', 'active', '2024-06-21 10:51:02', '2024-06-21 10:51:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bills`
+--
+
+CREATE TABLE `bills` (
+  `id` int NOT NULL,
+  `user_member_id` int NOT NULL,
+  `month` varchar(20) NOT NULL,
+  `year` int NOT NULL,
+  `time_period` int DEFAULT NULL,
+  `installment_number` int DEFAULT NULL,
+  `arrears` decimal(10,2) DEFAULT NULL,
+  `principal` decimal(10,2) DEFAULT NULL,
+  `interest` decimal(10,2) DEFAULT NULL,
+  `mandatory_saving` decimal(10,2) DEFAULT NULL,
+  `total_bill` decimal(10,2) DEFAULT NULL,
+  `payment_amount` decimal(10,2) DEFAULT NULL,
+  `remaining_arrears` decimal(10,2) DEFAULT NULL,
+  `status` enum('paid','unpaid') DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `bills`
+--
+
+INSERT INTO `bills` (`id`, `user_member_id`, `month`, `year`, `time_period`, `installment_number`, `arrears`, `principal`, `interest`, `mandatory_saving`, `total_bill`, `payment_amount`, `remaining_arrears`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Januari', 2024, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2024-06-20 15:29:18', '2024-06-20 15:29:18'),
+(2, 1, 'Februari', 2024, 24, 1, NULL, 250000.00, 120000.00, 25000.00, 395000.00, 395000.00, NULL, 'paid', '2024-06-20 15:30:33', '2024-06-20 15:30:33');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `member_profiles`
 --
 
@@ -34,31 +112,44 @@ CREATE TABLE `member_profiles` (
   `position` varchar(100) DEFAULT NULL,
   `work_unit_id` int NOT NULL,
   `address` text,
-  `phone_number` varchar(20) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL
+  `phone_number` varchar(15) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `photo_url` varchar(255) DEFAULT NULL,
+  `join_date` date NOT NULL,
+  `status` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `member_profiles`
 --
 
-INSERT INTO `member_profiles` (`id`, `user_member_id`, `full_name`, `position`, `work_unit_id`, `address`, `phone_number`, `email`) VALUES
-(1, 1, 'Moh Dwi Ramdhani', 'Tank', 1, 'Veteran', '082296064822', NULL),
-(2, 6, 'dhani', NULL, 3, NULL, NULL, NULL);
+INSERT INTO `member_profiles` (`id`, `user_member_id`, `full_name`, `position`, `work_unit_id`, `address`, `phone_number`, `email`, `photo_url`, `join_date`, `status`) VALUES
+(1, 1, 'Moh Dwi Ramdhani', 'Tank', 1, 'Veteran', '082295050505', 'ramabmp@gmail.com', 'https/snjdjasdajsnda.com', '2024-01-15', 'active');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `savings_balance`
+-- Table structure for table `organizations`
 --
 
-CREATE TABLE `savings_balance` (
+CREATE TABLE `organizations` (
   `id` int NOT NULL,
-  `user_member_id` int NOT NULL,
-  `principal_saving` decimal(10,2) DEFAULT '0.00',
-  `mandatory_saving` decimal(10,2) DEFAULT NULL,
-  `voluntary_saving` decimal(10,2) DEFAULT NULL
+  `role` enum('pengawas','pengurus','staff') NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `photo_url` varchar(255) DEFAULT NULL,
+  `description` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `organizations`
+--
+
+INSERT INTO `organizations` (`id`, `role`, `position`, `full_name`, `photo_url`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'pengurus', 'Ketua', 'Saleh Ban', 'httpsdsadas.com', 'sdsadas', '2024-06-21 03:49:58', '2024-06-21 03:49:58'),
+(2, 'staff', 'Kasir', 'yayayaya', NULL, NULL, '2024-06-21 03:50:18', '2024-06-21 03:50:18');
 
 -- --------------------------------------------------------
 
@@ -109,6 +200,26 @@ INSERT INTO `work_units` (`id`, `code`, `name`) VALUES
 --
 
 --
+-- Indexes for table `balance_savings`
+--
+ALTER TABLE `balance_savings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_member_id` (`user_member_id`);
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bills`
+--
+ALTER TABLE `bills`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_member_id` (`user_member_id`);
+
+--
 -- Indexes for table `member_profiles`
 --
 ALTER TABLE `member_profiles`
@@ -118,11 +229,10 @@ ALTER TABLE `member_profiles`
   ADD KEY `work_unit_id` (`work_unit_id`);
 
 --
--- Indexes for table `savings_balance`
+-- Indexes for table `organizations`
 --
-ALTER TABLE `savings_balance`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_member_id` (`user_member_id`);
+ALTER TABLE `organizations`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_members`
@@ -143,16 +253,34 @@ ALTER TABLE `work_units`
 --
 
 --
--- AUTO_INCREMENT for table `member_profiles`
+-- AUTO_INCREMENT for table `balance_savings`
 --
-ALTER TABLE `member_profiles`
+ALTER TABLE `balance_savings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `banners`
+--
+ALTER TABLE `banners`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `savings_balance`
+-- AUTO_INCREMENT for table `bills`
 --
-ALTER TABLE `savings_balance`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `bills`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `member_profiles`
+--
+ALTER TABLE `member_profiles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `organizations`
+--
+ALTER TABLE `organizations`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_members`
@@ -171,17 +299,23 @@ ALTER TABLE `work_units`
 --
 
 --
+-- Constraints for table `balance_savings`
+--
+ALTER TABLE `balance_savings`
+  ADD CONSTRAINT `balance_savings_ibfk_1` FOREIGN KEY (`user_member_id`) REFERENCES `user_members` (`id`);
+
+--
+-- Constraints for table `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`user_member_id`) REFERENCES `user_members` (`id`);
+
+--
 -- Constraints for table `member_profiles`
 --
 ALTER TABLE `member_profiles`
   ADD CONSTRAINT `member_profiles_ibfk_1` FOREIGN KEY (`user_member_id`) REFERENCES `user_members` (`id`),
   ADD CONSTRAINT `member_profiles_ibfk_2` FOREIGN KEY (`work_unit_id`) REFERENCES `work_units` (`id`);
-
---
--- Constraints for table `savings_balance`
---
-ALTER TABLE `savings_balance`
-  ADD CONSTRAINT `savings_balance_ibfk_1` FOREIGN KEY (`user_member_id`) REFERENCES `user_members` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
