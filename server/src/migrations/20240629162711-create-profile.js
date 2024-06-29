fb712bb9fe50886f405b9770724cb2f6ad0825be
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-06-30 00:27:41 +0800

    Add profile model

---
+++ server/src/migrations/20240629162711-create-profile.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      full_name: {
+        type: Sequelize.STRING
+      },
+      position: {
+        type: Sequelize.STRING
+      },
+      work_unit_id: {
+        type: Sequelize.INTEGER
+      },
+      address: {
+        type: Sequelize.TEXT
+      },
+      photo_url: {
+        type: Sequelize.STRING
+      },
+      join_date: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      birth_date: {
+        type: Sequelize.DATE
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
---
+++ server/src/models/profile.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Profile.init({
+    user_id: DataTypes.INTEGER,
+    full_name: DataTypes.STRING,
+    position: DataTypes.STRING,
+    work_unit_id: DataTypes.INTEGER,
+    address: DataTypes.TEXT,
+    photo_url: DataTypes.STRING,
+    join_date: DataTypes.DATE,
+    status: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    birth_date: DataTypes.DATE,
+    phone_number: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Profile',
+  });
+  return Profile;
+};
