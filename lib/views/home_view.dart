2379c15b109af368ce597d201f14e92f70abddf9
ThoriqW <thoriqwajedi88@gmail.com>
2024-07-02 23:30:30 +0800

    add page setting view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,15 +1,18 @@
+import 'package:aplikasi_kpri_mobile/providers/profile_provider.dart';
 import 'package:aplikasi_kpri_mobile/views/about_view.dart';
 import 'package:aplikasi_kpri_mobile/views/balance_view.dart';
 import 'package:aplikasi_kpri_mobile/views/help_view.dart';
 import 'package:aplikasi_kpri_mobile/views/loan_view.dart';
 import 'package:aplikasi_kpri_mobile/widgets/home_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final profileState = ref.watch(profileNotifierProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -60,198 +63,217 @@ class HomeView extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              alignment: Alignment.bottomCenter,
-              clipBehavior: Clip.none,
+      body: profileState.when(
+        data: (profile) {
+          return SingleChildScrollView(
+            child: Column(
               children: [
-                Container(
-                  color: Theme.of(context).colorScheme.primary,
-                  width: double.infinity,
-                  padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 70.0),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 40,
-                        backgroundColor:
-                            Theme.of(context).colorScheme.primaryContainer,
-                        child: CircleAvatar(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.surfaceVariant,
-                          radius: 37,
-                          child: Icon(
-                            Icons.person,
-                            size: 40,
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 16),
-                      const Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      color: Theme.of(context).colorScheme.primary,
+                      width: double.infinity,
+                      padding:
+                          const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 70.0),
+                      child: Row(
                         children: [
-                          Text(
-                            "Moh Thoriq Wajedi",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.white),
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            child: CircleAvatar(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.surfaceVariant,
+                              radius: 37,
+                              child: Icon(
+                                Icons.person,
+                                size: 40,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
                           ),
-                          Text(
-                            "7204080401990001",
-                            style: TextStyle(fontSize: 12, color: Colors.white),
-                          ),
-                          SizedBox(
-                            height: 4,
-                          ),
-                          Text(
-                            "Bina Marga Kab. Tolitoli",
-                            style: TextStyle(fontSize: 12, color: Colors.white),
-                          ),
+                          const SizedBox(width: 16),
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                profile.fullName,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: Colors.white),
+                              ),
+                              Text(
+                                profile.nip,
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                profile.address ?? "",
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.white),
+                              ),
+                            ],
+                          )
                         ],
-                      )
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: -70,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width - 32,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Color.fromRGBO(0, 0, 0, 0.1),
-                          blurRadius: 12,
-                          spreadRadius: 0,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
+                      ),
                     ),
-                    padding: const EdgeInsets.all(12.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          mainAxisSize: MainAxisSize.min,
+                    Positioned(
+                      bottom: -70,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 32,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color.fromRGBO(0, 0, 0, 0.1),
+                              blurRadius: 12,
+                              spreadRadius: 0,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            const Text(
-                              "Saldo Anggota",
-                              style: TextStyle(fontSize: 12),
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Saldo Anggota",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                const SizedBox(height: 4),
+                                const Text(
+                                  "Rp. 2,700,500",
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(height: 8),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .primaryContainer,
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 5.0,
+                                    horizontal: 12.0,
+                                  ),
+                                  child: Text(
+                                    profile.joinDate,
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                )
+                              ],
                             ),
-                            const SizedBox(height: 4),
-                            const Text(
-                              "Rp. 2,700,500",
-                              style: TextStyle(
-                                  fontSize: 24, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 8),
                             Container(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 5.0,
+                                horizontal: 12.0,
+                              ),
                               decoration: BoxDecoration(
                                 color: Theme.of(context)
                                     .colorScheme
                                     .primaryContainer,
                                 borderRadius: BorderRadius.circular(8),
                               ),
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 5.0,
-                                horizontal: 12.0,
-                              ),
-                              child: const Text(
-                                "Okt 2009 - Mei 2024",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontStyle: FontStyle.italic,
+                              child: Text(
+                                profile.status ?? "",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             )
                           ],
                         ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 5.0,
-                            horizontal: 12.0,
-                          ),
-                          decoration: BoxDecoration(
-                            color:
-                                Theme.of(context).colorScheme.primaryContainer,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: const Text(
-                            "AKTIF",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 64,
+                  margin: const EdgeInsets.only(top: 100),
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 8,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: <Widget>[
+                      HomeMenu(
+                        title: "Simpanan",
+                        image: "wallet.svg",
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const BalanceView(),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
+                          );
+                        },
+                      ),
+                      HomeMenu(
+                        title: "Pinjaman",
+                        image: "note.svg",
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoanView(),
+                            ),
+                          );
+                        },
+                      ),
+                      HomeMenu(
+                        title: "THR",
+                        image: "gift.svg",
+                        onTap: () {},
+                      ),
+                      HomeMenu(
+                        title: "Tentang",
+                        image: "about.svg",
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const AboutView(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                )
+                ),
               ],
             ),
-            Container(
-              width: MediaQuery.of(context).size.width - 64,
-              margin: const EdgeInsets.only(top: 100),
-              child: GridView.count(
-                crossAxisCount: 4,
-                mainAxisSpacing: 12,
-                crossAxisSpacing: 8,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                children: <Widget>[
-                  HomeMenu(
-                    title: "Simpanan",
-                    image: "wallet.svg",
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const BalanceView(),
-                        ),
-                      );
-                    },
-                  ),
-                  HomeMenu(
-                    title: "Pinjaman",
-                    image: "note.svg",
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LoanView(),
-                        ),
-                      );
-                    },
-                  ),
-                  HomeMenu(
-                    title: "THR",
-                    image: "gift.svg",
-                    onTap: () {},
-                  ),
-                  HomeMenu(
-                    title: "Tentang",
-                    image: "about.svg",
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AboutView(),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          );
+        },
+        error: (error, stackTrace) {
+          return Center(
+            child: Text("Failed to load home: $error"),
+          );
+        },
+        loading: () {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
---
+++ lib/views/settings_view.dart
@@ -0,0 +1,61 @@
+import 'package:aplikasi_kpri_mobile/widgets/switch_setting.dart';
+import 'package:flutter/material.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Pengaturan",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width - 32,
+              margin: const EdgeInsets.only(top: 20),
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 12),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                    blurRadius: 9,
+                    spreadRadius: 0,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Notifikasi",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SwitchSetting()
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
