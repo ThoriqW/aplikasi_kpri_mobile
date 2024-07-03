b59b1f49cff4299413d9dacb98d62338f6683ba9
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-03 13:48:18 +0800

    update seeder for users

--- server/seeders/20240629135045-demo-user.js
+++ server/seeders/20240629135045-demo-user.js
@@ -6,30 +6,47 @@ const saltRounds = 10;
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const hashedPassword1 = await bcrypt.hash('123123', saltRounds);
-    const hashedPassword2 = await bcrypt.hash('123123', saltRounds);
-    const hashedPassword3 = await bcrypt.hash('199', saltRounds);
+    const hashedPassword = await bcrypt.hash('123123', saltRounds);
+    const defaultPassword = await bcrypt.hash('kprikd', saltRounds);
 
-    await queryInterface.bulkInsert('Users', [
+    // Pengguna tetap
+    const users = [
       {
         nip: '190302029',
-        password: hashedPassword1,
+        password: hashedPassword,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         nip: '190302021',
-        password: hashedPassword2,
+        password: hashedPassword,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-      {
-        nip: '199',
-        password: hashedPassword3,
+    ];
+
+    const generateRandomNIP = () => {
+      const yearOfBirth = '1985';
+      const monthOfBirth = '07';
+      const dayOfBirth = '23';
+      const yearOfPNS = '2005';
+      const monthOfPNS = '02';
+      const gender = Math.floor(Math.random() * 2) + 1;
+      const orderNumber = String(Math.floor(Math.random() * 900) + 100).padStart(3, '0');
+
+      return `${yearOfBirth}${monthOfBirth}${dayOfBirth}${yearOfPNS}${monthOfPNS}${gender}${orderNumber}`;
+    };
+
+    for (let i = 0; i < 30; i++) {
+      users.push({
+        nip: generateRandomNIP(),
+        password: defaultPassword,
         createdAt: new Date(),
         updatedAt: new Date(),
-      },
-    ]);
+      });
+    }
+
+    await queryInterface.bulkInsert('Users', users, {});
   },
 
   async down(queryInterface, Sequelize) {
