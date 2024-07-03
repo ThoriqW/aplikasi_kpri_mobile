402d81ae307143b75c57c76988112eb2816b7cf3
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-03 14:06:40 +0800

    update seeder for bills

--- server/seeders/20240701060721-demo-bill.js
+++ server/seeders/20240701060721-demo-bill.js
@@ -1,11 +1,12 @@
 'use strict';
 
+const faker = require('@faker-js/faker').fakerID_ID;
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const bills = [];
 
-    // Helper function to add bill
     const addBill = (userId, month, year, mandatory, principal = 0, interest = 0, timePeriod = 0, installmentNumber = 0, arrears = 0, paymentAmount = 25000, status = 'Sudah Terbayar') => {
       const totalBill = principal + interest + mandatory + arrears;
       bills.push({
@@ -26,56 +27,46 @@ module.exports = {
       });
     };
 
-    // Create bills for user_id: 1
-    const startYear1 = 2022;
-    const endYear1 = 2024;
-    let installmentNumber = 0;
+    for (let userId = 1; userId <= 32; userId++) {
+      const startYear = faker.datatype.number({ min: 2019, max: 2023 });
+      const endYear = 2024;
 
-    for (let year = startYear1; year <= endYear1; year++) {
-      const startMonth = (year === startYear1) ? 2 : 1;
-      const endMonth = (year === endYear1) ? 7 : 12;
+      let installmentNumber = 0;
 
-      for (let month = startMonth; month <= endMonth; month++) {
-        let principal = 0;
-        let interest = 0;
-        let timePeriod = 0;
-        let status = 'Sudah Terbayar';
-        let paymentAmount = 25000;
+      for (let year = startYear; year <= endYear; year++) {
+        const startMonth = (year === startYear) ? faker.datatype.number({ min: 1, max: 12 }) : 1;
+        const endMonth = (year === endYear) ? 7 : 12;
 
-        if (year === 2024 && month >= 4) {
-          timePeriod = 12;
-          installmentNumber++;
-          principal = 250000;
-          interest = 120000;
-          if (installmentNumber === 2) {
-            status = 'Belum Terbayar';
-            paymentAmount = 25000;
-          } else if (installmentNumber === 3) {
-            status = 'Sudah Terbayar';
+        for (let month = startMonth; month <= endMonth; month++) {
+          let principal = 0;
+          let interest = 0;
+          let timePeriod = 0;
+          let status = 'Sudah Terbayar';
+          let paymentAmount = 25000;
+
+          if (year === 2024 && month >= 4) {
+            timePeriod = 12;
+            installmentNumber++;
+            principal = faker.datatype.number({ min: 120000, max: 300000 });
+            interest = faker.datatype.number({ min: 50000, max: 150000 });
+
+            if (installmentNumber === 2) {
+              status = 'Belum Terbayar';
+              paymentAmount = 25000;
+            } else if (installmentNumber === 3) {
+              status = 'Sudah Terbayar';
+            }
           }
+
+          addBill(userId, new Date(year, month - 1, 1).toLocaleString('id-ID', { month: 'long' }), year, 25000, principal, interest, timePeriod, installmentNumber, 0, paymentAmount, status);
         }
-
-        addBill(1, new Date(2024, month - 1, 1).toLocaleString('id-ID', { month: 'long' }), year, 25000, principal, interest, timePeriod, installmentNumber, 0, paymentAmount, status);
-      }
-    }
-
-    // Create bills for user_id: 2
-    const startYear2 = 2021;
-    const endYear2 = 2024;
-
-    for (let year = startYear2; year <= endYear2; year++) {
-      const startMonth = (year === startYear2) ? 1 : 1;
-      const endMonth = (year === endYear2) ? 7 : 12;
-
-      for (let month = startMonth; month <= endMonth; month++) {
-        addBill(2, new Date(year, month - 1, 1).toLocaleString('id-ID', { month: 'long' }), year, 25000);
       }
     }
 
     await queryInterface.bulkInsert('Bills', bills);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Bills', null, {});
   }
 };
